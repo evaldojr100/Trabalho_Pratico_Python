472bba22467fc47217d31c3b192fe72aed8f781c
evaldojr100 <evaldojr_melo@hotmail.com>
2018-11-28 21:39:25 -0400

    Adicionando e lendo Itens

--- Agenda.py
+++ Agenda.py
@@ -4,6 +4,19 @@ for i in range(2):
     texto =input("Digite seu nome:")
     telefone = input("Digite o telefone de %s:"%(texto))
     arquivo.writelines((texto,"\t",telefone,"\n"))
-
-
 arquivo.close()
+arquivo= open("dados.csv","r")
+for linha in arquivo.readlines():
+    print (linha)
+arquivo.close()
+
+arquivo= open("dados.csv","w")
+arquivo.writelines(("Luiz\t33212589\n"))
+arquivo.close()
+
+arquivo= open("dados.csv","r")
+for linha in arquivo.readlines():
+    print (linha)
+arquivo.close()
+
+
