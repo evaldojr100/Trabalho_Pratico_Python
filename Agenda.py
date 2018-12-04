6c06d8f6cfbb1bb216a39cf27e3c570cd5875379
evaldojr100 <evaldojr_melo@hotmail.com>
2018-12-03 22:10:46 -0400

    Alterar Valor

--- Agenda.py
+++ Agenda.py
@@ -1,22 +1,49 @@
 import csv
 
-lista=[]
+arquivo_csv=[]
 def entrada_dados():
     linha=[]
+    cod=len(arquivo_csv)+1
     texto = input("Digite seu nome:")
     telefone = input("Digite o telefone de %s:" % (texto))
-    linha=[texto,telefone]
-    lista.append(linha)
+    linha=[cod,texto,telefone]
+    arquivo_csv.append(linha)
     gravar()
-def gravar():
+def gravar(lista):
     with open("dados.csv","w") as arquivo:
         writer = csv.writer(arquivo, delimiter=',')
         writer.writerows(lista)
 def ler_tabela():
     with open("dados.csv", "r") as arquivo:
-        dados = list(csv.reader(arquivo, delimiter=','))
+        dados = list((csv.reader(arquivo, delimiter=',')))
         arquivo.close()
         return dados
+def consultar():
+    lista = ler_tabela()
+    texto = input("Digite o nome a pesquisar:")
+    for i in lista:
+        if texto in i[1]:
+            return i
+
+def alterar():
+    lista=ler_tabela()
+    dado=consultar()
+    print("\nNome:%s, Telefone:%s\n"%(dado[1],dado[2]))
+    print("Se deseja alterar o nome digite 1")
+    print("Se deseja alterar o telefone digite 2")
+    print("Se deseja alterar o nome e telefone digite 3")
+    aux=int(input("Sua escolha:"))
+    if aux==1:
+        dado[1]=input("Digite o novo Nome:")
+    elif aux==2:
+        dado[2]=input("Digite o novo Telefone:")
+    elif aux==3:
+        dado[1] = input("Digite o novo Nome:")
+        dado[2] = input("Digite o novo Telefone:")
+    lista[0]=dado
+
+    gravar(lista)
+
 
 
 '''opcao=0
@@ -32,7 +59,7 @@ while opcao==0:
         opcao=0'''
 
 
-
+alterar()
 
 
 
