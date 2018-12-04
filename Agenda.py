399fe239c0e1f9bb4c0311cbbcc382c63f677095
evaldojr100 <evaldojr_melo@hotmail.com>
2018-12-03 21:32:47 -0400

    Setando Metodos

--- Agenda.py
+++ Agenda.py
@@ -1,20 +1,54 @@
 import csv
 
-def inserir():
-    arquivo = open("dados.csv", "w")
+lista=[]
+def entrada_dados():
+    linha=[]
     texto = input("Digite seu nome:")
     telefone = input("Digite o telefone de %s:" % (texto))
-    arquivo.writelines((texto, "\t", telefone, "\n"))
-    arquivo.close()
+    linha=[texto,telefone]
+    lista.append(linha)
+    gravar()
+def gravar():
+    with open("dados.csv","w") as arquivo:
+        writer = csv.writer(arquivo, delimiter=',')
+        writer.writerows(lista)
+def ler_tabela():
+    with open("dados.csv", "r") as arquivo:
+        dados = list(csv.reader(arquivo, delimiter=','))
+        arquivo.close()
+        return dados
 
-def procurar():
-    arquivo = open("dados.csv", "r")
-    lista = list(csv.reader(arquivo, delimiter=','))
-    arquivo.close()
-    return lista
+
+'''opcao=0
+while opcao==0:
+    print("Digite 1 para Inserir Dados")
+    print("Digite 2 para Pesquisar um cadastro")
+    print("Digite 3 para Alterar Um cadastro")
+    print("Digite 4 para Excluir um Cadastro")
+    opcao = int(input("Sua Escolha:"))
+    
+    if opcao==1:
+        entrada_dados()
+        opcao=0'''
 
 
-print(lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
